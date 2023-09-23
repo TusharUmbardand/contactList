fa6a0a0b2700332a4d60afda4a76364b67d577c9
TusharUmbardand <tusharumbardand1@gmail.com>
2023-09-23 21:14:19 +0530

    day 1

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # contactList
 This is my first repo
 <br>
-Author :- Tushar Umbardand
+Author :- Tushar Umbardand 
+23-09-2023
+first time adding changes to my project.
+to add changed file of updated file we can use  (add filename)
---
+++ index.java
@@ -0,0 +1,8 @@
+class index{
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
+//ls : to all the files present inside the folder 
+// status : it is used to indecate there is change in code on git and your local machine if it is there 
